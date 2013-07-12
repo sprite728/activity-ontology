3f4c88f343d715029693749bcb5bb715670f7d1d
git <sebastianclaici@gmail.com>
2013-07-12 12:52:02 +0200

    ADD: Ruby script to delete HITs from MTurk

---
+++ scripts/delete_hits.rb
@@ -0,0 +1,22 @@
+#!/usr/bin/env ruby
+
+# Deletes all HITs (even those that have not been reviewed)
+
+rootdir = File.dirname $0
+login = rootdir + "/login.rb"
+
+require login
+
+hits = RTurk::Hit.all
+
+unless hits.empty?
+    puts "Removing all HITs"
+
+    hits.each do |hit|
+        hit.expire!
+        hit.assignments.each do |assignment|
+            assignment.approve!
+        end
+        hit.dispose!
+    end
+end
