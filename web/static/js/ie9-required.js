c35d041dadb1cc4e20703c726cfb8869e3b5b2be
Sebastian Claici <sebastianclaici@gmail.com>
2013-08-06 14:18:49 +0200

    FIX: Fixed issue where users could submit without checking any of the time of day checkboxes.

--- web/static/js/ie9-required.js
+++ web/static/js/ie9-required.js
@@ -1,23 +1,35 @@
 $('#submit-form').submit(function() {
     var emptyPred = $('#predicate').val().length === 0;
-    var emptyObj = $('#object').val().length === 0; 
+    var emptyObj = $('#object').val().length === 0;
     var emptyDur = $('#duration').prop('selectedIndex') === -1;
-    var emptyTime = $('#time').val() === null;
 
-    var emptyMore = true;
+    // Check "time" checkboxes are not empty
+    var emptyTime = true;
     var i = 0;
+    var checkboxes = document.getElementsByName("time");
+    while (emptyTime && i < checkboxes.length) {
+        if (checkboxes[i].checked) {
+            emptyTime = false;
+        }
+        ++i;
+    }
+
+    // Check "more" radio field is not empty
+    var emptyMore = true;
+    i = 0;
     var radios = document.getElementsByName("more");
     while (emptyMore && i < radios.length) {
-       if (radios[i].checked)
+       if (radios[i].checked) {
           emptyMore = false;
+       }
        ++i;
     }
 
-    var isFormValid = !emptyObj && !emptyPred && 
+    var isFormValid = !emptyObj && !emptyPred &&
           !emptyDur && !emptyTime && !emptyMore;
 
     if (!isFormValid) {
-        alert("Please fill in all the fields!");
+        alert("Please fill in all the fields.");
     }
 
     return isFormValid;
