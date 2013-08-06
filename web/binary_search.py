81749697b59a9940cc642da6f723a0f75d455810
Sebastian Claici <sebastianclaici@gmail.com>
2013-08-06 14:18:14 +0200

    ADD: Changed the look and feel of the two templates (location_base and location2).

--- web/binary_search.py
+++ web/binary_search.py
@@ -3,7 +3,7 @@
 from bisect import bisect_left
 
 def binary_search(a, val):
-    lo, hi = 0, len(a)
     pos = bisect_left(a, val)
 
+    hi = len(a)
     return (pos if pos != hi and a[pos] == val else -1)
