6bad54267ed3531df48d2924933b20fc2862895e
Sebastian Claici <sebastianclaici@gmail.com>
2013-08-09 15:57:09 +0200

    ADD: Modifications to the scripts for robustness and extra features.
    normalize_results.py:
        - added replacement of objects that match WordNet objects
        - added function to take a data set and return CSV data
        - added function to count number of Yes vs. No votes
    
    approve_hits.rb:
        - added functionality to iterate over more than 100 HITs
        - added rejection of incorrect results
    
    publish_external.rb:
        - added command-line arguments for common parameters (number HITs,
          batches, etc.)

--- scripts/HIT_scripts/approve_hits.rb
+++ scripts/HIT_scripts/approve_hits.rb
@@ -1,23 +1,49 @@
 #!/usr/bin/env ruby
 
-# Approves and gets the results from all hits
-
 rootdir = File.dirname $0
 login = rootdir + "/login.rb"
 
 require login
 require "yaml"
 
-hits = RTurk::Hit.all_reviewable
+# Modify Hit class to allow iterating over more than 100 results
+module RTurk
+  class Hit
+    def self.each(page_number=1, &block)
+      results = RTurk::SearchHITs.create(page_number:page_number, page_size:100)
+      num_results = results.xpath("//TotalNumResults").text.to_i
 
-unless hits.empty?
-  puts "Reviewing and approving all assignments"
+      results.hits.map do |hit|
+        yield new(hit.id, hit)
+      end
+      each(page_number + 1, &block) if num_results > page_number * 100
+    end
+  end
+end
+
+# Approve all HITs that are correct
+def approve
+  puts "Reviewing and approving assignments"
 
   File.open('results/results.yaml', 'w') do |file|
-    hits.each do |hit|
+    RTurk::Hit.each do |hit|
       hit.assignments.each do |assignment|
+        # Write result to YAML file
         file.write(YAML::dump(assignment.answers))
-        assignment.approve! if assignment.status == 'Submitted'
+
+        location = assignment.answers['location']
+        honeypot = assignment.answers['honeypot']
+
+        # Check if honeypot answer is correct. If not, reject HIT
+        if location != honeypot
+          if location == 'rv_park' || location == 'atm'
+            assignment.approve! if assignment.status == 'Submitted'
+          else
+            assignment.reject! if assignment.status == 'Submitted'
+          end
+        else
+          assignment.approve! if assignment.status == 'Submitted'
+        end
       end
     end
   end
--- scripts/HIT_scripts/publish_external.rb
+++ scripts/HIT_scripts/publish_external.rb
@@ -11,55 +11,59 @@ def createNewExternalQuestion
   keywords = "activity, recognition, location, mapping"
   numAssignments = 1
 
-  numHits = -1
+  numHits = 0
+  batches = 1
   rewardAmount = 0.03
   base = 'http://crowd.fooshed.net/location/'
 
-  if ARGV.length == 1
-    numHits = ARGV[0].to_i
-  elsif ARGV.length == 2
-    numHits = ARGV[0].to_i
-    rewardAmount = ARGV[1].to_f
-  elsif ARGV.length == 3
-    numHits = ARGV[0].to_i
-    rewardAmount = ARGV[1].to_f
-    base = ARGV[2]
+  # Update vars with command line arguments if any
+  if ARGV.length >= 1
+    numHits = ARGV[0].to_i unless ARGV[0] == 'ALL'
+  end
+  if ARGV.length >= 2
+    batches = ARGV[1].to_i
+  end
+  if ARGV.length >= 3
+    rewardAmount = ARGV[2].to_f
+  end
+  if ARGV.length == 4
+    base = ARGV[3]
   end
 
-  p numHits
-  p rewardAmount
-  p base
+  # Publish in batches
+  batches.times do
+    locations = IO.read(Dir.pwd + "/misc/locs.txt").split()
+    locations.map! { |x| x.strip() }
 
-  locations = IO.read(Dir.pwd + "/misc/locs.txt").split()
-  locations.map! { |x| x.strip() }
-
-  locations.sort_by! { rand }
-  # Publish only numHits HITs
-  locations = locations.slice(0..numHits-1)
+    locations.sort_by! { rand }
+    # Publish only numHits HITs
+    locations = locations.slice(0..numHits-1)
 
 
-  # Wrap HIT into an ExternalQuestion form and send it to MTurk
-  locations.each do |location|
-    link = base + location
-    p link
-    question = "<ExternalQuestion xmlns=\"http://mechanicalturk" +
-      ".amazonaws.com/AWSMechanicalTurkDataSchemas/" +
-      "2006-07-14/ExternalQuestion.xsd\">\n"
-    question += "<ExternalURL>" + link + "</ExternalURL>\n"
-    question += "<FrameHeight>600</FrameHeight>\n"
-    question += "</ExternalQuestion>\n"
+    # Wrap HIT into an ExternalQuestion form and send it to MTurk
+    locations.each do |location|
+      link = base + location
+      question = "<ExternalQuestion xmlns=\"http://mechanicalturk" +
+        ".amazonaws.com/AWSMechanicalTurkDataSchemas/" +
+        "2006-07-14/ExternalQuestion.xsd\">\n"
+      question += "<ExternalURL>" + link + "</ExternalURL>\n"
+      question += "<FrameHeight>600</FrameHeight>\n"
+      question += "</ExternalQuestion>\n"
 
-    result = @mturk.createHIT(:Title => title,
-                              :Description => desc,
-                              :MaxAssignment => numAssignments,
-                              :Reward => { :Amount => rewardAmount,
-                                           :CurrencyCode => 'USD' },
-                              :Question => question.strip(),
-                              :Keywords => keywords )
+      result = @mturk.createHIT(:Title => title,
+                                :Description => desc,
+                                :MaxAssignment => numAssignments,
+                                :Reward => { :Amount => rewardAmount,
+                                  :CurrencyCode => 'USD' },
+                                  :Question => question.strip(),
+                                  :Keywords => keywords )
 
-    puts "Created HIT: #{result[:HITId]}"
-    puts "HIT Location: #{getHITUrl(result[:HITTypeId])}"
+      puts "Created HIT: #{result[:HITId]}"
+      puts "HIT Location: #{getHITUrl(result[:HITTypeId])}"
+    end
   end
+
+  puts "Published #{count} HITs (#{count / numHits} batches)"
 end
 
 def getHITUrl( hitTypeId )
@@ -70,7 +74,8 @@ def getHITUrl( hitTypeId )
   end
 end
 
-USAGE = 'USAGE: publish_external [NUM HITs] [REWARD] [BASE_URL]'
+### Usage data
+USAGE = 'USAGE: publish_external [NUM HITs] [BATCHES] [REWARD] [BASE_URL]'
 
 if ARGV.length > 0 && ARGV[0] == '--help'
   puts USAGE
